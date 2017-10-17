8fc6fb4433834bf7b5f812b41fde8a66a170283b
chrish <chrish@yxunistar.com>
2017-10-17 11:11:37 +0800

    Add samples.

--- app/src/main/java/io/csie/chris/androidrxjavaworks/MainActivity.java
+++ app/src/main/java/io/csie/chris/androidrxjavaworks/MainActivity.java
@@ -5,9 +5,14 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import io.reactivex.Flowable;
+import io.reactivex.Observable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +31,53 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Don't show anything
+        Flowable.just("Hello world1");
+
+        // Hello world2 shows
+        Flowable.just("Hello world2").subscribe(System.out::println);
+
+        // Without java8
+        // Hello world3 shows
+        Flowable.just("Hello world3")
+                .subscribe(new Consumer<String>() {
+                    @Override
+                    public void accept(String s) {
+                        System.out.println(s);
+                    }
+                });
+
+        // Don't show anything
+        Observable<String> myObservable = Observable.just("Hello world3");
+
+        // Hello world4 shows
+        Disposable myObservable1 = Observable.just("Hello world4").subscribe(System.out::println);
+
+        // Hello world5 777 shows
+        Disposable myObservable2 = Observable.just("Hello world5")
+                .map(s -> s + " 777")
+                .subscribe(System.out::println);
+
+        Disposable myObservable4 = Observable.just("Hello world6")
+                .map(String::hashCode)
+                .subscribe(i -> System.out.println(Integer.toString(i)));
+
+        Disposable myObservable5 = Observable.just("Hello world7")
+                .map(String::hashCode)
+                .map(i -> Integer.toString(i))
+                .subscribe(System.out::println);
+
+        Disposable myObservable6 = Observable.just("Hello world8")
+                .map(s -> s + " 999")
+                .map(String::hashCode)
+                .map(i -> Integer.toString(i))
+                .subscribe(System.out::println);
     }
 
     @Override
